60c085adda15b5bb365dc065681aa71de389f5c8
R_N_A <33271130+RNA4219@users.noreply.github.com>
2025-10-20 23:26:18 +0900

    Handle boxed primitives in stable stringify

---
+++ tests/stable-stringify-boxed-primitives.test.ts
@@ -0,0 +1,22 @@
+import test from "node:test";
+import assert from "node:assert";
+
+import { Cat32, stableStringify } from "../src/index.js";
+
+test("stableStringify distinguishes boxed primitive numbers", () => {
+  const boxedOne = Object(1);
+  const boxedTwo = Object(2);
+
+  assert.ok(stableStringify(boxedOne) !== stableStringify(boxedTwo));
+});
+
+test("Cat32 assign key reflects boxed boolean value", () => {
+  const cat = new Cat32();
+
+  const trueAssignment = cat.assign(Object(true));
+  const falseAssignment = cat.assign(Object(false));
+
+  assert.ok(trueAssignment.key !== falseAssignment.key);
+  assert.ok(trueAssignment.key === "true");
+  assert.ok(falseAssignment.key === "false");
+});
