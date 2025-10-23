14201cbbc2fdf8b3fe0b6111c95850ec19e77b6e
R_N_A <33271130+RNA4219@users.noreply.github.com>
2025-10-23 15:25:26 +0900

    Move dist ambient declarations under type root

--- types/dist-modules.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "../dist/categorizer.js" {
-  export type { NormalizeMode, CategorizerOptions, Assignment } from "../src/categorizer";
-  export { Cat32 } from "../src/categorizer";
-}
-
-declare module "../dist/hash.js" {
-  export { fnv1a32, toHex32 } from "../src/hash";
-}
