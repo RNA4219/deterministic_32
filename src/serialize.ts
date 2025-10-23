048822040a9d764c4262e5d2adf1d774ce167ba3
R_N_A <33271130+RNA4219@users.noreply.github.com>
2025-10-23 14:26:44 +0900

    Fix SymbolObject alias for TypeScript 5

--- src/serialize.ts
+++ src/serialize.ts
@@ -34,7 +34,7 @@ function isBigIntObject(value: unknown): value is { valueOf(): bigint } {
 
 type ValueOfCapable = { valueOf(): unknown };
 
-type SymbolObject = symbol & object;
+type SymbolObject = Symbol;
 
 type LocalSymbolHolder = {
   symbol: symbol;
